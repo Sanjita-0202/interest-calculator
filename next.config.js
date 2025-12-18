e71fac5db5c1d6cb332f41ad3e7a1ef098293b36
Sanjita-0202 <sanjita.prakash2024@vitstudent.ac.in>
2025-12-18 19:32:33 +0530

    Fix Next.js config: remove invalid experimental.appDir

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
+  output: "standalone", // server mode, NOT static export
 };
 
 module.exports = nextConfig;
